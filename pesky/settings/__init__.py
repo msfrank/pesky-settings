63c7de8c9e43b81694af9a158e86d64dbfb63755
Michael Frank <msfrank@syntaxjockey.com>
2014-05-30 10:48:56 -0700

    bump dev version to 0.0.3

--- pesky/settings/__init__.py
+++ pesky/settings/__init__.py
@@ -3,7 +3,7 @@
 # This file is part of Pesky.  Pesky is BSD-licensed software;
 # for copyright information see the LICENSE file.
 
-__version__ = (0, 0, 2)
+__version__ = (0, 0, 3)
 
 def versionstring():
     """
